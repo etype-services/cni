1797070ac043408a074add2173de3b0f4c5c25f0
Charlie Llewellin <charliellewellin@gmail.com>
2015-11-04 18:02:49 +0000

    merge

--- sites/all/libraries/ckeditor/plugins/dialog/dialogDefinition.js
+++ sites/all/libraries/ckeditor/plugins/dialog/dialogDefinition.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 ﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
+ Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-=======
-﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
->>>>>>> origin/master
