e132eb2faeab29c2b8ce3d5e41f83f78dfe8618b
Charlie Llewellin <charliellewellin@gmail.com>
2015-12-27 20:10:57 +0000

    adding login page

--- sites/all/themes/cni/js/user-menu.js
+++ sites/all/themes/cni/js/user-menu.js
@@ -2,7 +2,9 @@
     Drupal.behaviors.usermenu = {
         attach: function (context) {
             $('#block-system-user-menu ul li').each(function(index) {
-                console.log( index + ": " + $( this ).text() );
+                if ($(this).text() == 'Log In') {
+                    $(this).hide();
+                }
             })
         }
     };
--- sites/all/themes/cni/template.php
+++ sites/all/themes/cni/template.php
@@ -75,8 +75,12 @@ function cni_preprocess_html(&$vars) {
     $vars['classes_array'][] = drupal_clean_css_identifier($alias);
   }
 
+  /*
+   * Hide Log In link from logged in users
+   * A call to hook_menu_alter in etype.module does not do the trick
+   */
   global $user;
-  if ($user->uid  > 0) {
+  if ($user->uid > 0) {
     drupal_add_js('sites/all/themes/cni/js/user-menu.js', 'file');
   }
 
