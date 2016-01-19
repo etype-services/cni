29749839b8bd733299780bd7c6ad6b4a5261f666
Charlie Llewellin <charliellewellin@gmail.com>
2016-01-19 17:55:57 -0600

    sub link in article template

--- sites/default/themes/cni/templates/node--article.tpl.php
+++ sites/default/themes/cni/templates/node--article.tpl.php
@@ -46,7 +46,7 @@
       ?>
     </div>
     
-    <div class="view-more-digital"><a href="http://www.etypeservices.com/Cherokee%20ScoutID600/">To view more please log in or subscribe to the digital edition.</a></div>
+    <div class="view-more-digital"><a href="http://www.etypeservices.com/Times-CourierID603">To view more please log in or subscribe to the digital edition.</a></div>
     
     <?php if (isset($content['links'])): ?>  
       <?php print render($content['links']); ?>
