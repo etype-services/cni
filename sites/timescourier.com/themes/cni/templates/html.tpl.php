66c3d2e18de23f0b913860f900d41537c9db925d
Charlie Llewellin <charliellewellin@gmail.com>
2016-01-26 17:58:11 -0600

    facebook block

--- sites/timescourier.com/themes/cni/templates/html.tpl.php
+++ sites/timescourier.com/themes/cni/templates/html.tpl.php
@@ -20,13 +20,6 @@
     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));</script>
-<script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
