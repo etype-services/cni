17e9026b81f10c1c0c849461033b0eb589184125
Charlie Llewellin <charliellewellin@gmail.com>
2016-02-25 09:48:00 -0600

    nassau county more link

--- sites/nassaucountyrecord.com/themes/cni/templates/node--article.tpl.php
+++ sites/nassaucountyrecord.com/themes/cni/templates/node--article.tpl.php
@@ -46,7 +46,7 @@
       ?>
     </div>
     
-    <div class="view-more-digital"><a href="http://www.etypeservices.com/Fernandina%20Beach%20News-LeaderID565/">To view more please log in or subscribe to the digital edition.</a></div>
+    <div class="view-more-digital"><a href="http://www.etypeservices.com/Nassau%20County%20RecordID609">To view more please log in or subscribe to the digital edition.</a></div>
     
     <?php if (isset($content['links'])): ?>  
       <?php print render($content['links']); ?>
