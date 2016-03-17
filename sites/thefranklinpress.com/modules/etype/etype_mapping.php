6d5a421aa771e32e4cdb4cdae1e78cc7831216c0
Charlie Llewellin <charliellewellin@gmail.com>
2016-03-17 10:07:00 -0500

    franklin press classifieds

--- sites/thefranklinpress.com/modules/etype/etype_mapping.php
+++ sites/thefranklinpress.com/modules/etype/etype_mapping.php
@@ -1,7 +1,7 @@
 <?php
 
 /* import _url */
-$url = 'http://www.etypeclassifieds.com/cherokeexml/CNI_CSC.xml';
+$url = 'http://www.etypeclassifieds.com/frnkpressxml/CNI_FRP.xml';
 
 /* ad categories */
 $categories = array(
