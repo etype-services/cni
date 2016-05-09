6c5ca71ade876d2a8dceeaf8efc761d76e90d435
Charlie Llewellin <charliellewellin@gmail.com>
2016-05-09 15:50:43 -0500

    press-sentinel

--- sites/sites.php
+++ sites/sites.php
@@ -43,161 +43,111 @@
 $sites['nassaucountyrecord.etypegoogle3.com'] = 'nassaucountyrecord.com';
 $sites['nassaucountyrecord.com'] = 'nassaucountyrecord.com';
 $sites['www.nassaucountyrecord.com'] = 'nassaucountyrecord.com';
-$sites['prod.nassaucountyrecord.com'] = 'nassaucountyrecord.com';
-$sites['dev.nassaucountyrecord.com'] = 'nassaucountyrecord.com';
 
 $sites['grahamstar.etypegoogle3.com'] = 'grahamstar.com';
 $sites['grahamstar.com'] = 'grahamstar.com';
 $sites['www.grahamstar.com'] = 'grahamstar.com';
-$sites['prod.grahamstar.com'] = 'grahamstar.com';
-$sites['dev.grahamstar.com'] = 'grahamstar.com';
 
 $sites['whitecountynews.etypegoogle3.com'] = 'whitecountynews.net';
 $sites['whitecountynews.net'] = 'whitecountynews.net';
 $sites['www.whitecountynews.net'] = 'whitecountynews.net';
-$sites['prod.whitecountynews.net'] = 'whitecountynews.net';
-$sites['dev.whitecountynews.net'] = 'whitecountynews.net';
 
 $sites['tribune-georgian.etypegoogle3.com'] = 'tribune-georgian.com';
 $sites['tribune-georgian.com'] = 'tribune-georgian.com';
 $sites['www.tribune-georgian.com'] = 'tribune-georgian.com';
-$sites['prod.tribune-georgian.com'] = 'tribune-georgian.com';
-$sites['dev.tribune-georgian.com'] = 'tribune-georgian.com';
 
 $sites['thetoccoarecord.etypegoogle3.com'] = 'thetoccoarecord.com';
 $sites['thetoccoarecord.com'] = 'thetoccoarecord.com';
 $sites['www.thetoccoarecord.com'] = 'thetoccoarecord.com';
-$sites['prod.thetoccoarecord.com'] = 'thetoccoarecord.com';
-$sites['dev.thetoccoarecord.com'] = 'thetoccoarecord.com';
 
 $sites['thenortheastgeorgian.etypegoogle3.com'] = 'thenortheastgeorgian.com';
 $sites['thenortheastgeorgian.com'] = 'thenortheastgeorgian.com';
 $sites['www.thenortheastgeorgian.com'] = 'thenortheastgeorgian.com';
-$sites['prod.thenortheastgeorgian.com'] = 'thenortheastgeorgian.com';
-$sites['dev.thenortheastgeorgian.com'] = 'thenortheastgeorgian.com';
 
 $sites['myandrewsjournal.etypegoogle3.com'] = 'myandrewsjournal.com';
 $sites['myandrewsjournal.com'] = 'myandrewsjournal.com';
 $sites['www.myandrewsjournal.com'] = 'myandrewsjournal.com';
-$sites['prod.myandrewsjournal.com'] = 'myandrewsjournal.com';
-$sites['dev.myandrewsjournal.com'] = 'myandrewsjournal.com';
 
 $sites['lakecityreporter.etypegoogle3.com'] = 'lakecityreporter.com';
 $sites['lakecityreporter.com'] = 'lakecityreporter.com';
 $sites['www.lakecityreporter.com'] = 'lakecityreporter.com';
-$sites['prod.lakecityreporter.com'] = 'lakecityreporter.com';
-$sites['dev.lakecityreporter.com'] = 'lakecityreporter.com';
 
 $sites['thehartwellsun.etypegoogle3.com'] = 'thehartwellsun.com';
 $sites['thehartwellsun.com'] = 'thehartwellsun.com';
 $sites['www.thehartwellsun.com'] = 'thehartwellsun.com';
-$sites['prod.thehartwellsun.com'] = 'thehartwellsun.com';
-$sites['dev.thehartwellsun.com'] = 'thehartwellsun.com';
  
 $sites['franklincountycitizen.etypegoogle3.com'] = 'franklincountycitizen.com';
 $sites['franklincountycitizen.com'] = 'franklincountycitizen.com';
 $sites['www.franklincountycitizen.com'] = 'franklincountycitizen.com';
-$sites['prod.franklincountycitizen.com'] = 'franklincountycitizen.com';
-$sites['dev.franklincountycitizen.com'] = 'franklincountycitizen.com';
 
 $sites['fbnewsleader.etypegoogle3.com'] = 'fbnewsleader.com';
 $sites['fbnewsleader.com'] = 'fbnewsleader.com';
 $sites['www.fbnewsleader.com'] = 'fbnewsleader.com';
-$sites['prod.fbnewsleader.com'] = 'fbnewsleader.com';
-$sites['dev.fbnewsleader.com'] = 'fbnewsleader.com';
 
 $sites['elberton.etypegoogle3.com'] = 'elberton.com';
 $sites['elberton.com'] = 'elberton.com';
 $sites['www.elberton.com'] = 'elberton.com';
-$sites['prod.elberton.com'] = 'elberton.com';
-$sites['dev.elberton.com'] = 'elberton.com';
 
 $sites['thedahloneganugget.etypegoogle3.com'] = 'thedahloneganugget.com';
 $sites['thedahloneganugget.com'] = 'thedahloneganugget.com';
 $sites['www.thedahloneganugget.com'] = 'thedahloneganugget.com';
-$sites['prod.thedahloneganugget.com'] = 'thedahloneganugget.com';
-$sites['dev.thedahloneganugget.com'] = 'thedahloneganugget.com';
 
 $sites['theclaytontribune.etypegoogle3.com'] = 'theclaytontribune.com';
 $sites['theclaytontribune.com'] = 'theclaytontribune.com';
 $sites['www.theclaytontribune.com'] = 'theclaytontribune.com';
-$sites['prod.theclaytontribune.com'] = 'theclaytontribune.com';
-$sites['dev.theclaytontribune.com'] = 'theclaytontribune.com';
 
 $sites['cherokeescout.etypegoogle3.com'] = 'cherokeescout.com';
 $sites['cherokeescout.com'] = 'cherokeescout.com';
 $sites['www.cherokeescout.com'] = 'cherokeescout.com';
-$sites['prod.cherokeescout.com'] = 'cherokeescout.com';
-$sites['dev.cherokeescout.com'] = 'cherokeescout.com';
 
 $sites['beachesleader.etypegoogle3.com'] = 'beachesleader.com';
 $sites['beachesleader.com'] = 'beachesleader.com';
 $sites['www.beachesleader.com'] = 'beachesleader.com';
-$sites['prod.beachesleader.com'] = 'beachesleader.com';
-$sites['dev.beachesleader.com'] = 'beachesleader.com';
  
 $sites['thenewsobserver.etypegoogle3.com'] = 'thenewsobserver.com';
 $sites['thenewsobserver.com'] = 'thenewsobserver.com';
 $sites['www.thenewsobserver.com'] = 'thenewsobserver.com';
-$sites['prod.thenewsobserver.com'] = 'thenewsobserver.com';
-$sites['dev.thenewsobserver.com'] = 'thenewsobserver.com';
 
 $sites['claycountyprogress.etypegoogle3.com'] = 'claycountyprogress.com';
 $sites['claycountyprogress.com'] = 'claycountyprogress.com';
 $sites['www.claycountyprogress.com'] = 'claycountyprogress.com';
-$sites['prod.claycountyprogress.com'] = 'claycountyprogress.com';
-$sites['dev.claycountyprogress.com'] = 'claycountyprogress.com';
 
 $sites['timescourier.etypegoogle3.com'] = 'timescourier.com';
 $sites['timescourier.com'] = 'timescourier.com';
 $sites['www.timescourier.com'] = 'timescourier.com';
-$sites['prod.timescourier.com'] = 'timescourier.com';
-$sites['dev.timescourier.com'] = 'timescourier.com';
 
 $sites['gracefulcarega.etypegoogle3.com'] = 'gracefulcarega.com';
 $sites['gracefulcarega.com'] = 'gracefulcarega.com';
 $sites['www.gracefulcarega.com'] = 'gracefulcarega.com';
-$sites['prod.gracefulcarega.com'] = 'gracefulcarega.com';
-$sites['dev.gracefulcarega.com'] = 'gracefulcarega';
 
 $sites['thefranklinpress.etypegoogle3.com'] = 'thefranklinpress.com';
 $sites['thefranklinpress.com'] = 'thefranklinpress.com';
 $sites['www.thefranklinpress.com'] = 'thefranklinpress.com';
-$sites['prod.thefranklinpress.com'] = 'thefranklinpress.com';
-$sites['dev.thefranklinpress.com'] = 'thefranklinpress.com';
 
 $sites['highlandsleadingagents.etypegoogle3.com'] = 'highlandsleadingagents.com';
 $sites['highlandsleadingagents.com'] = 'highlandsleadingagents.com';
 $sites['www.highlandsleadingagents.com'] = 'highlandsleadingagents.com';
-$sites['prod.highlandsleadingagents.com'] = 'highlandsleadingagents.com';
-$sites['dev.highlandsleadingagents.com'] = 'highlandsleadingagents.com';
 
 $sites['highlandsnews.etypegoogle3.com'] = 'highlandsnews.com';
 $sites['highlandsnews.com'] = 'highlandsnews.com';
 $sites['www.highlandsnews.com'] = 'highlandsnews.com';
-$sites['prod.highlandsnews.com'] = 'highlandsnews.com';
-$sites['dev.highlandsnews.com'] = 'highlandsnews.com';
 
 $sites['crossroadschronicle.etypegoogle3.com'] = 'crossroadschronicle.com';
 $sites['crossroadschronicle.com'] = 'crossroadschronicle.com';
 $sites['www.crossroadschronicle.com'] = 'crossroadschronicle.com';
-$sites['prod.crossroadschronicle.com'] = 'crossroadschronicle.com';
-$sites['dev.crossroadschronicle.com'] = 'crossroadschronicle.com';
 
 $sites['mitchellnews.etypegoogle3.com'] = 'mitchellnews.com';
 $sites['mitchellnews.com'] = 'mitchellnews.com';
 $sites['www.mitchellnews.com'] = 'mitchellnews.com';
-$sites['prod.mitchellnews.com'] = 'mitchellnews.com';
-$sites['dev.mitchellnews.com'] = 'mitchellnews.com';
 
 $sites['thesmokymountaintimes.etypegoogle3.com'] = 'thesmokymountaintimes.com';
 $sites['thesmokymountaintimes.com'] = 'thesmokymountaintimes.com';
 $sites['www.thesmokymountaintimes.com'] = 'thesmokymountaintimes.com';
-$sites['prod.thesmokymountaintimes.com'] = 'thesmokymountaintimes.com';
-$sites['dev.thesmokymountaintimes.com'] = 'thesmokymountaintimes.com';
 
 $sites['palatkadailynews.etypegoogle3.com'] = 'palatkadailynews.com';
 $sites['palatkadailynews.com'] = 'palatkadailynews.com';
 $sites['www.palatkadailynews.com'] = 'palatkadailynews.com';
-$sites['prod.palatkadailynews.com'] = 'palatkadailynews.com';
-$sites['dev.palatkadailynews.com'] = 'palatkadailynews.com';
+
+$sites['thepress-sentinel.etypegoogle3.com'] = 'thepress-sentinel.com';
+$sites['thepress-sentinel.com'] = 'thepress-sentinel.com';
+$sites['www.thepress-sentinel.com'] = 'thepress-sentinel.com';
