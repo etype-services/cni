226dfafd1081eb9c2b3a337e69271eb50eab9d4c
Charlie Llewellin <charliellewellin@gmail.com>
2016-05-10 14:30:53 -0500

    press-sentinel

--- sites/thepress-sentinel.com/themes/cni/templates/html.tpl.php
+++ sites/thepress-sentinel.com/themes/cni/templates/html.tpl.php
@@ -12,6 +12,14 @@
 </head>
 
 <body class="<?php print $classes; ?>"<?php print $attributes;?>>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
