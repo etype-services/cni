c72cf09a31c889a24d7f6cb35aa7667dad587ccb
Charlie Llewellin <charlie@llewellin.net>
2017-02-24 12:42:29 -0600

    cherokee scout

--- sites/cherokeescout.com/etype_settings.php
+++ sites/cherokeescout.com/etype_settings.php
@@ -3,6 +3,9 @@
 /* import _url */
 $url = 'http://www.etypeclassifieds.com/cherokeexml/CNI_CSC.xml';
 
+/* cron schedule */
+$cron_schedule = '30 7 * * 3';
+
 /* ad categories */
 $categories = array(
 
