08880b76956492e280ec0f81319cb7279a263047
Charlie Llewellin <charlie@llewellin.net>
2017-03-16 11:44:57 -0500

    user menu

--- sites/all/themes/cni/template.php
+++ sites/all/themes/cni/template.php
@@ -39,6 +39,8 @@ function cni_preprocess_node(&$variables) {
 
     case 'custom-login-page':
       $replace_file = $path . 'login.tpl.php';
+      echo $replace_file;
+      exit;
       break;
 
     case 'forgot-password':
