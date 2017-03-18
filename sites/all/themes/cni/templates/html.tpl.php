0cda9cd7de5357e691b93f0e5b6a724cbccf34df
Charlie Llewellin <charlie@llewellin.net>
2017-03-18 14:00:16 -0500

    main menu

--- sites/all/themes/cni/templates/html.tpl.php
+++ sites/all/themes/cni/templates/html.tpl.php
@@ -1,19 +1,12 @@
 <!DOCTYPE html>
-<html lang="<?php print $language->language; ?>"
-      dir="<?php print $language->dir; ?>" <?php print $rdf_namespaces; ?>>
-
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>" <?php print $rdf_namespaces; ?>>
 <head>
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
     <?php print $scripts; ?>
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet'
-          type='text/css'>
-    <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 </head>
 
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
