791268b124491f2427c5ecd04a516765ca23418c
Charlie Llewellin <charlie@llewellin.net>
2017-03-20 13:34:30 -0500

    main menu

--- sites/all/themes/cni/js/scripts.js
+++ sites/all/themes/cni/js/scripts.js
@@ -1,39 +1,4 @@
 (function ($) {
-
-    Drupal.behaviors.bonesSuperfish = {
-
-        attach: function (context, settings) {
-
-            $('#user-menu ul.menu', context).superfish({
-                delay: 400,
-                animation: {height: 'show'},
-                speed: 500,
-                easing: 'easeOutBounce',
-                autoArrows: false,
-                dropShadows: false /* Needed for IE */
-            });
-
-        }
-    }
-
-    $(function () {
-
-        $('.postscript-wrapper img').hover(function () {
-            $(this).animate({
-                backgroundColor: "#ff7800", opacity: "1.0"
-            }, 'fast');
-        }, function () {
-            $(this).animate({
-                backgroundColor: "#555", opacity: "0.9"
-            }, 'normal');
-        });
-
-    });
-
-
-})(jQuery);
-
-(function ($) {
     Drupal.behaviors.superfish = {
         attach: function (context) {
 
