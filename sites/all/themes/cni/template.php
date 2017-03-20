543c60ae1ffd2d901ce760e9bf4e6f11893da953
Charlie Llewellin <charlie@llewellin.net>
2017-03-20 13:15:23 -0500

    main menu

--- sites/all/themes/cni/template.php
+++ sites/all/themes/cni/template.php
@@ -6,32 +6,32 @@ function cni_preprocess_page(&$variables) {
   $grid_info = get_grid_info();
 
   // Create page variables
-  $vars['grid_size'] = 'container_' . $grid_info['grid_size'];
-  $vars['grid_full_width'] = 'grid_' . $grid_info['grid_size'];
-  $vars['sidebar_first_grid_width'] = 'grid_' . $grid_info['sidebar_first_width'];
-  $vars['sidebar_second_grid_width'] = 'grid_' . $grid_info['sidebar_second_width'];
-  $vars['twitter'] = theme_get_setting('twitter');
-  $vars['facebook'] = theme_get_setting('facebook');
+  $variables['grid_size'] = 'container_' . $grid_info['grid_size'];
+  $variables['grid_full_width'] = 'grid_' . $grid_info['grid_size'];
+  $variables['sidebar_first_grid_width'] = 'grid_' . $grid_info['sidebar_first_width'];
+  $variables['sidebar_second_grid_width'] = 'grid_' . $grid_info['sidebar_second_width'];
+  $variables['twitter'] = theme_get_setting('twitter');
+  $variables['facebook'] = theme_get_setting('facebook');
 
   for ($region_count = 1; $region_count <= 4; $region_count++) {
-    $vars['preface_' . $region_count . '_grid_width'] = 'grid_' . $grid_info['preface_' . $region_count . '_grid_width'];
-    $vars['postscript_' . $region_count . '_grid_width'] = 'grid_' . $grid_info['postscript_' . $region_count . '_grid_width'];
+    $variables['preface_' . $region_count . '_grid_width'] = 'grid_' . $grid_info['preface_' . $region_count . '_grid_width'];
+    $variables['postscript_' . $region_count . '_grid_width'] = 'grid_' . $grid_info['postscript_' . $region_count . '_grid_width'];
   }
 
-  if (empty($vars['page']['sidebar_first']) && empty($vars['page']['sidebar_second'])) {
-    $vars['main_content_grid_width'] = 'grid_' . $grid_info['grid_size'];
+  if (empty($variables['page']['sidebar_first']) && empty($variables['page']['sidebar_second'])) {
+    $variables['main_content_grid_width'] = 'grid_' . $grid_info['grid_size'];
   }
   else {
-    if (!empty($vars['page']['sidebar_first']) && !empty($vars['page']['sidebar_second'])) {
-      $vars['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - ($grid_info['sidebar_first_width'] + $grid_info['sidebar_second_width']));
+    if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+      $variables['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - ($grid_info['sidebar_first_width'] + $grid_info['sidebar_second_width']));
     }
     else {
-      if (empty($vars['page']['sidebar_first']) && !empty($vars['page']['sidebar_second'])) {
-        $vars['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - $grid_info['sidebar_second_width']);
+      if (empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+        $variables['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - $grid_info['sidebar_second_width']);
       }
       else {
-        if (!empty($vars['page']['sidebar_first']) && empty($vars['page']['sidebar_second'])) {
-          $vars['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - $grid_info['sidebar_first_width']);
+        if (!empty($variables['page']['sidebar_first']) && empty($variables['page']['sidebar_second'])) {
+          $variables['main_content_grid_width'] = 'grid_' . ($grid_info['grid_size'] - $grid_info['sidebar_first_width']);
         }
       }
     }
@@ -107,18 +107,18 @@ function cni_form_alter(&$form, &$form_state, $form_id) {
 }
 
 /**
- * @param $vars
+ * @param $variables
  */
-function cni_preprocess_html(&$vars) {
+function cni_preprocess_html(&$variables) {
 
   // Add body class for sidebar layout
-  $vars['classes_array'][] = theme_get_setting('sidebar_layout');
+  $variables['classes_array'][] = theme_get_setting('sidebar_layout');
 
   /* add body classes */
   $path = drupal_get_path_alias($_GET['q']);
   $aliases = explode('/', $path);
   foreach ($aliases as $alias) {
-    $vars['classes_array'][] = drupal_clean_css_identifier($alias);
+    $variables['classes_array'][] = drupal_clean_css_identifier($alias);
   }
 
   /* add site-specific css */
