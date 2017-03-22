82a0c30db40af4881d7f08fe089f049db233dfbc
Charlie Llewellin <charlie@llewellin.net>
2017-03-22 13:28:14 -0500

    main menu

--- sites/all/themes/cni/js/scripts.js
+++ sites/all/themes/cni/js/scripts.js
@@ -2,9 +2,8 @@
     Drupal.behaviors.superfish = {
         attach: function (context) {
 
-            function mobile_menu(w) {
+            function mobile_menu(w, searchForm) {
                 var wrapper = $('#mobile-menu-wrapper');
-                var searchForm = $('#search-block-form');
 
                 if (w > 767) {
                     if ( wrapper.length > 0 ) {
@@ -15,34 +14,36 @@
                     if ( wrapper.length === 0 ) {
                         $('.region-user-menu').prepend('<div id="mobile-menu-wrapper"><div id="mobile-menu-control"></div><ul id="mobile-menu" class="menu"></ul></div>');
 
-                        var search = $('#search-control');
                         searchForm.hide();
-                        if ( search.length === 0 ) {
-                            $('#block-search-form .content').append('<div id="search-control"></div>');
-                            var search = $('#search-control');
-                        }
-
                         $('#superfish-1 li').clone().appendTo($('#mobile-menu'));
                         $('#block-system-user-menu ul.menu li').clone().appendTo($('#mobile-menu'));
                         $('#mobile-menu li ul').remove();
                         $('#mobile-menu-control').click(function(){$('#mobile-menu').toggle()});
 
-                        $(search).click(function(){
-                            searchForm.toggle();
-                            if(searchForm.is(':visible')) {
-                                $('#edit-search-block-form--2').focus();
-                            }
-                        });
                     }
                 }
-            };
+            }
+
+            var searchForm = $('#search-block-form');
+            var search = $('#search-control');
+            if ( search.length === 0 ) {
+                $('#block-search-form .content').append('<div id="search-control"></div>');
+                var search = $('#search-control');
+            }
+
+            $(search).click(function(){
+                searchForm.toggle();
+                if(searchForm.is(':visible')) {
+                    $('#edit-search-block-form--2').focus();
+                }
+            });
 
             var w = $(window).width();
-            mobile_menu(w);
+            mobile_menu(w, searchForm);
 
             $(window).resize(function () {
                 var w = $(window).width();
-                mobile_menu(w);
+                mobile_menu(w, searchForm);
             });
         }
     };
