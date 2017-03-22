d7f75663abea2442d76b0e409321232b904492d0
Charlie Llewellin <charlie@llewellin.net>
2017-03-22 13:02:09 -0500

    main menu

--- sites/all/themes/cni/js/scripts.js
+++ sites/all/themes/cni/js/scripts.js
@@ -11,15 +11,17 @@
                 } else {
                     if ( wrapper.length === 0 ) {
                         $('.region-user-menu').prepend('<div id="mobile-menu-wrapper"><div id="mobile-menu-control"></div><ul id="mobile-menu" class="menu"></ul></div>');
-                        var search = $('#search-control');
                         var searchForm = $('#search-block-form');
                         if ( search.length === 0 ) {
                             $('#block-search-form .content').append('<div id="search-control"></div>');
                         }
+
                         $('#superfish-1 li').clone().appendTo($('#mobile-menu'));
                         $('#block-system-user-menu ul.menu li').clone().appendTo($('#mobile-menu'));
                         $('#mobile-menu li ul').remove();
                         $('#mobile-menu-control').click(function(){$('#mobile-menu').toggle()});
+
+                        var search = $('#search-control');
                         $(search).click(function(){
                             searchForm.toggle();
                             if(searchForm.is(':visible')) {
