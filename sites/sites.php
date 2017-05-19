1265b3710a79d9a0758199b5c568d5164dcbd151
Charlie Llewellin <charlie@llewellin.net>
2017-05-19 09:16:03 -0500

    wire

--- sites/sites.php
+++ sites/sites.php
@@ -156,7 +156,7 @@ $sites['unwindnorthgeorgia.etypegoogle3.com'] = 'unwindnorthgeorgia.etypegoogle3
 $sites['unwindnorthgeorgia.com'] = 'unwindnorthgeorgia.etypegoogle3.com';
 $sites['www.unwindnorthgeorgia.com'] = 'unwindnorthgeorgia.etypegoogle3.com';
 
-$sites['test.etypegoogle3.com'] = 'mitchellnews.com';
+$sites['test.etypegoogle3.com'] = 'thenortheastgeorgian.com';
 $sites['coupons.etypegoogle3.com'] = 'palatkadailynews.com';
 
 $sites['cninewspapers.etypegoogle3.com'] = 'cninewspapers.etypegoogle3.com';
